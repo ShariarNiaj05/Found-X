dcc1623631b901f19d971d6341da94ca36c3a61f
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-10 00:45:13 +0600

    route grouping

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,10 +1,16 @@
+import { Navbar } from "@/src/components/navbar";
+
 import { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
-      <h1>Inside Route Group</h1>
-      {children}
+      <div className="relative flex flex-col h-screen">
+        <Navbar />
+        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
