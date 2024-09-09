11fcd31dc30d3b6a9aa4e8117e115903f19b835f
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-10 00:00:00 +0600

    clean navbar

--- src/config/site.ts
+++ src/config/site.ts
@@ -32,11 +32,4 @@ export const siteConfig = {
       href: "/found-items",
     },
   ],
-  links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
 };
