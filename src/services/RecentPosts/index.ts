c5ad8bf8d75693490cea34c9b207129944922901
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-10 02:24:27 +0600

    services logic

--- src/services/RecentPosts/index.ts
+++ src/services/RecentPosts/index.ts
@@ -1,6 +1,6 @@
+import envConfig from "@/src/config/envConfig";
+
 export const getRecentPost = async () => {
-  const res = await fetch(
-    "http://localhost:5000/api/v1/items?sort=-createdAt&limit=9"
-  );
+  const res = await fetch(`${envConfig.baseApi}/items?sort=-createdAt&limit=9`);
   return res.json();
 };
