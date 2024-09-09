78b4c84384615e780f25ece43ba1ac8ceb8420a3
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-09 23:42:26 +0600

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import { nextui } from '@nextui-org/theme'
 module.exports = {
   content: [
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
