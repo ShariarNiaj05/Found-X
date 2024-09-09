760d22000d39dd271eff35d7f54f3489c6fe0dc5
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-10 00:04:51 +0600

    customizing home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,27 +10,7 @@ import { GithubIcon } from "@/src/components/icons";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
-      </div>
-
-      <div className="flex gap-3"></div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+      <h1>Found X</h1>
     </section>
   );
 }
