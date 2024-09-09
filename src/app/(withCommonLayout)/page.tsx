90e1a5467311fb5b7aef912df64c59a49ba98c80
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-10 01:17:11 +0600

    Home page design

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-[calc(100vh-64px)] bg-url[('./glass.jpg')]">
+    <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')]">
       <h1>Found X</h1>
     </div>
   );
