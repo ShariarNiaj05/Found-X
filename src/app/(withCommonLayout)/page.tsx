c63ff3918abd1f65564a56cddec430e600e36232
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-10 01:30:11 +0600

    Home page design

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,8 +2,8 @@ import { Input } from "@nextui-org/input";
 
 export default function Home() {
   return (
-    <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
+    <>
       <Input />
-    </div>
+    </>
   );
 }
