13c3a9e4cde1a3945fd7964bae750bbaad425695
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-11 23:51:40 +0600

    Implementing parallel routes

---
+++ src/app/(withCommonLayout)/(home)/@recentPosts/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="bg-red-200 h-56 w-36">
+      <div className="section-title my-8 h-48 w-36"></div>
+    </div>
+  );
+};
+
+export default loading;
