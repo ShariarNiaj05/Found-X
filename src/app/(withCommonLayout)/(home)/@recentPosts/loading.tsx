e1a13397e530ed72b123bf0def63c5365b0cd672
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-11 23:57:22 +0600

    loading skeleton

--- src/app/(withCommonLayout)/(home)/@recentPosts/loading.tsx
+++ src/app/(withCommonLayout)/(home)/@recentPosts/loading.tsx
@@ -1,8 +1,23 @@
 const loading = () => {
   return (
-    <div className="bg-red-200 h-56 w-36 p-20">
-      <div className="section-title my-8 h-48 w-36"></div>
-    </div>
+    <Container>
+      <div className="section-title my-8">
+        <h2 className="mb-2 text-center text-2xl">Recently Found Items</h2>
+        <p className="text-center">
+          A list of items that have been recently found and reported.
+        </p>
+      </div>
+      <div className="my-8 grid justify-center gap-10 sm:grid-cols-1 md:grid-cols-4">
+        {[...Array(9)].map((item: IPost) => (
+          <Card key={item._id} post={item} />
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <Button className="rounded-md bg-default-900 text-default" size="md">
+          <Link href="/found-items">See All</Link>
+        </Button>
+      </div>
+    </Container>
   );
 };
 
