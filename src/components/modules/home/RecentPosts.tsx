a6319bf3a3b3f4a5f28c8fdff11c243c568ad52e
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-11 22:14:32 +0600

    Refactoring item card and item data

--- src/components/modules/home/RecentPosts.tsx
+++ src/components/modules/home/RecentPosts.tsx
@@ -2,6 +2,7 @@ import { Button } from "@nextui-org/button";
 import Link from "next/link";
 import Container from "../../UI/Container";
 import { getRecentPosts } from "@/src/services/RecentPosts";
+import Card from "../../UI/Card";
 
 export default async function RecentPosts() {
   const { data: posts } = await getRecentPosts();
@@ -16,7 +17,7 @@ export default async function RecentPosts() {
       </div>
       <div className="my-8 grid justify-center gap-10 sm:grid-cols-1 md:grid-cols-4">
         {posts.map((item) => (
-          <p>{item.title}</p>
+          <Card key={item._id} post={item} />
         ))}
       </div>
       <div className="flex justify-center">
