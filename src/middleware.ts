9d74803a25deb63a25f36b8c340e3a80e917fadc
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 00:19:49 +0600

    user checks and role-based routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import type { NextRequest } from "next/server";
 
 const AuthRoutes = ["/login", "/register"];
 
+const roleBasedRoutes = {
+  USER: [],
+  ADMIN: [],
+};
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
