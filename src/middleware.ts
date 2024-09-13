f5d556e5e320631f717ee739eaea0a2e692b4840
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 00:02:37 +0600

    user checks and role-based routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,12 @@ const AuthRoutes = ["/login", "/register"];
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const pathName = request.nextUrl;
   const user = undefined;
   if (!user) {
+    if (AuthRoutes.includes(pathName)) {
+      return NextResponse.redirect(new URL(pathName, request.url));
+    }
   }
 
   return NextResponse.redirect(new URL("/", request.url));
