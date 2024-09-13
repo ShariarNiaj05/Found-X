58b621dee074e6b2b5a868033f7dd7b9235cc6bd
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 01:10:07 +0600

    react hook form wrapper component

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
+      <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
+      <p className="mb-4">Welcome Back! Let&lsquo;s Get Started</p>
+      <div className="w-[35%]">
+        <div className="text-center">
+          Don&lsquo;t have account ? <Link href={"/register"}>Register</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
