9697133322c8340c00ec036d3d16cce74793d914
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-13 23:49:37 +0600

    middleware for authenticcation and authorization

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,11 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const user = {
+    name: "Shariar",
+    token: "token",
+    role: "USER",
+  };
   return NextResponse.redirect(new URL("/", request.url));
 }
 
