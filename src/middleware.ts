5b6d931b2030e916c4bbffc574eab13cc7fa8098
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-13 23:57:00 +0600

    user checks and role-based routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,9 @@ const AuthRoutes = ["/login", "/register"];
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  /* const user = {
-    name: "Shariar",
-    token: "token",
-    role: "USER",
-  }; */
+  const user = undefined;
+  if (!user) {
+  }
 
   return NextResponse.redirect(new URL("/", request.url));
 }
