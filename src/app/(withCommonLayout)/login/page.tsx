25e256198b332c6307098e1544c73e58db9ce6a9
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 22:20:52 +0600

    Connecting NextUI input with React Hook Form

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -1,3 +1,5 @@
+import FXForm from "@/src/components/form/FXForm";
+import { Button } from "@nextui-org/button";
 import Link from "next/link";
 
 const LoginPage = () => {
@@ -6,6 +8,25 @@ const LoginPage = () => {
       <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
       <p className="mb-4">Welcome Back! Let&lsquo;s Get Started</p>
       <div className="w-[35%]">
+        <FXForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(loginValidationSchema)}
+        >
+          <div className="py-3">
+            <FXInput name="email" label="Email" type="email" />
+          </div>
+          <div className="py-3">
+            <FXInput name="password" label="Password" type="password" />
+          </div>
+
+          <Button
+            className="my-3 w-full rounded-md bg-default-900 font-semibold text-default"
+            size="lg"
+            type="submit"
+          >
+            Login
+          </Button>
+        </FXForm>
         <div className="text-center">
           Don&lsquo;t have account ? <Link href={"/register"}>Register</Link>
         </div>
