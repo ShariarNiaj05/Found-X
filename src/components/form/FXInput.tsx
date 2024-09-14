59a27a2999f53ba88cb8ea5a43ed875790991c06
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 22:24:47 +0600

    Connecting NextUI input with React Hook Form

--- src/components/form/FXInput.tsx
+++ src/components/form/FXInput.tsx
@@ -1,5 +1,16 @@
 const FXInput = () => {
-  return <div>FXInput</div>;
+  return (
+    <Input
+      {...register(name)}
+      errorMessage={errors[name] ? (errors[name].message as string) : ""}
+      isInvalid={!!errors[name]}
+      variant={variant}
+      size={size}
+      required={required}
+      type={type}
+      label={label}
+    />
+  );
 };
 
 export default FXInput;
