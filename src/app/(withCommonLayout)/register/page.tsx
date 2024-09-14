f962b5b6d051e5a034f6a152f77532c2ee982662
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 23:28:13 +0600

    Creating user registration server action using Axios instance

--- src/app/(withCommonLayout)/register/page.tsx
+++ src/app/(withCommonLayout)/register/page.tsx
@@ -1,3 +1,5 @@
+import FXForm from "@/src/components/form/FXForm";
+import FXInput from "@/src/components/form/FXInput";
 import Link from "next/link";
 
 export default function page() {
@@ -6,6 +8,43 @@ export default function page() {
       <h3 className="my-2 text-xl font-bold">Register with FoundX</h3>
       <p className="mb-4">Help Lost Items Find Their Way Home</p>
       <div className="w-[35%]">
+        <FXForm
+          //! Only for development
+          defaultValues={{
+            name: "Shariar Islam",
+            email: "shariar@gmail.com",
+            mobileNumber: "01711223344",
+            password: "123456",
+          }}
+          resolver={zodResolver(registerValidationSchema)}
+          onSubmit={onSubmit}
+        >
+          <div className="py-3">
+            <FXInput label="Name" name="name" size="sm" />
+          </div>
+          <div className="py-3">
+            <FXInput label="Email" name="email" size="sm" />
+          </div>
+          <div className="py-3">
+            <FXInput label="Mobile Number" name="mobileNumber" size="sm" />
+          </div>
+          <div className="py-3">
+            <FXInput
+              label="Password"
+              name="password"
+              size="sm"
+              type="password"
+            />
+          </div>
+
+          <Button
+            className="my-3 w-full rounded-md bg-default-900 text-default"
+            size="lg"
+            type="submit"
+          >
+            Registration
+          </Button>
+        </FXForm>
         <div className="text-center">
           Already have an account ? <Link href={"/login"}>Login</Link>
         </div>
