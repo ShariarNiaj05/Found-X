611b0909fc0b7f9299011c633e81fa554ea259ff
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 23:45:31 +0600

    Creating user registration server action using Axios instance

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,6 +1,10 @@
 "use server";
+
+import axiosInstance from "@/src/lib/AxiosInstance";
+
 export const registerUser = async () => {
   try {
+    const { data } = await axiosInstance.post("/auth/register");
   } catch (error: any) {
     throw new Error(error);
   }
