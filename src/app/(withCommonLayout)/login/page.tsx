112982a7e7461cc1ad535b93c3015c7fc92bfa40
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 22:21:43 +0600

    Connecting NextUI input with React Hook Form

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -1,8 +1,12 @@
 import FXForm from "@/src/components/form/FXForm";
 import { Button } from "@nextui-org/button";
 import Link from "next/link";
+import { FieldValues, SubmitHandler } from "react-hook-form";
 
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
       <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
