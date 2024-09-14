e5a4fc2ca095e02952d3d5202c27a6bce91852c3
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 22:25:43 +0600

    Connecting NextUI input with React Hook Form

--- src/components/form/FXInput.tsx
+++ src/components/form/FXInput.tsx
@@ -1,4 +1,22 @@
-const FXInput = () => {
+import { Input } from "@nextui-org/input";
+
+interface IProps {
+  variant?: "flat" | "bordered" | "faded" | "underlined";
+  size?: "sm" | "md" | "lg";
+  required?: boolean;
+  type?: string;
+  label: string;
+  name: string;
+}
+
+const FXInput = ({
+  variant = "bordered",
+  size = "md",
+  required = false,
+  type = "text",
+  label,
+  name,
+}: IProps) => {
   return (
     <Input
       {...register(name)}
