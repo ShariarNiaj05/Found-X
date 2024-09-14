667f7ad84202f4366d6456fe17079176ac229499
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-15 00:41:15 +0600

    Creating a mutation hook for user registration

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { NextUIProvider } from "@nextui-org/system";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -20,6 +21,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <NextUIProvider navigate={router.push}>
+        <Toaster />
         <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
       </NextUIProvider>
     </QueryClientProvider>
