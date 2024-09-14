fd0c658300dce8f16fb31d52b63d0a4ed24c7115
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-15 00:12:38 +0600

    Next.js cookies and Tanstack Query

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { NextUIProvider } from "@nextui-org/system";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -14,9 +15,13 @@ export interface ProvidersProps {
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
+  const queryClient = new QueryClient();
+
   return (
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider navigate={router.push}>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
