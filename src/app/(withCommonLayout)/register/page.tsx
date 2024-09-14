d23399a667ca9a606035fd18a326a57ad77790cb
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 23:26:38 +0600

    Creating user registration server action using Axios instance

--- src/app/(withCommonLayout)/register/page.tsx
+++ src/app/(withCommonLayout)/register/page.tsx
@@ -1,7 +1,13 @@
 export default function page() {
   return (
-    <div>
-      <h1> This is Register page </h1>
+    <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
+      <h3 className="my-2 text-xl font-bold">Register with FoundX</h3>
+      <p className="mb-4">Help Lost Items Find Their Way Home</p>
+      <div className="w-[35%]">
+        <div className="text-center">
+          Already have an account ? <Link href={"/login"}>Login</Link>
+        </div>
+      </div>
     </div>
   );
 }
