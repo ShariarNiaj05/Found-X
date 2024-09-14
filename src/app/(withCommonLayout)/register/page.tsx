0ec3b22804ae113545334e41a5c7b427bc1ada0f
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-14 23:49:11 +0600

    Creating user registration server action using Axios instance

--- src/app/(withCommonLayout)/register/page.tsx
+++ src/app/(withCommonLayout)/register/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
     };
 
-    console.log("Inside form user data: ", data);
+    console.log("Inside form user data: ", userData);
   };
   return (
     <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -6,6 +6,7 @@ import { FieldValues } from "react-hook-form";
 export const registerUser = async (userData: FieldValues) => {
   try {
     const { data } = await axiosInstance.post("/auth/register", userData);
+    console.log(data);
   } catch (error: any) {
     throw new Error(error);
   }
