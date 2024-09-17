3391584451a689c0554a3febdb93160907bc3259
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-18 00:25:54 +0600

    glassmorphism-style loading spinner

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
   };
   return (
     <>
-      <div className="bg-green-500/20  h-screen fixed inset-0 z[99] backdrop-blur-md"></div>
+      <div className="bg-green-500/10  h-screen fixed inset-0 z[99] backdrop-blur-md"></div>
       <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
         <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
         <p className="mb-4">Welcome Back! Let&lsquo;s Get Started</p>
