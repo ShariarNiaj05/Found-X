f2235fed14c0880653eea95f65f67f91379a1393
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-18 00:59:52 +0600

    Fetching user data using the accessToken

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@
 import type { NextRequest } from "next/server";
 
 import { NextResponse } from "next/server";
+import { getCurrentUser } from "./services/AuthService";
 
 const AuthRoutes = ["/login", "/register"];
 
@@ -13,7 +14,7 @@ const roleBasedRoutes = {
 type Role = keyof typeof roleBasedRoutes;
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   console.log(pathname);
@@ -23,7 +24,7 @@ export function middleware(request: NextRequest) {
     token: "adsf asda",
     role: "USER",
   }; */
-
+  const user = await getCurrentUser();
   const user = undefined;
 
   if (!user) {
