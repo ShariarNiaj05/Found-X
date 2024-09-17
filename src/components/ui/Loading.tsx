acc5ff8291ea2928f1e37e41256faac5c798e207
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-18 00:36:27 +0600

    glassmorphism-style loading spinner

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,3 +1,5 @@
+import { Spinner } from "@nextui-org/spinner";
+
 const Loading = () => {
   return (
     <div className="bg-black/10  h-screen fixed inset-0 z[99] backdrop-blur-md flex justify-center items-center">
