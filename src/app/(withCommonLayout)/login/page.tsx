4dd9f7fd32cc49078fa4777b9af6c4a4978eca1a
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-18 00:40:43 +0600

    glassmorphism-style loading spinner

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -9,15 +9,16 @@ import FXInput from "@/src/components/form/FXInput";
 import FXForm from "@/src/components/form/FXForm";
 import { loginValidationSchema } from "@/src/schemas/login.schema";
 import { Spinner } from "@nextui-org/spinner";
+import { useUserLogin } from "@/src/hooks/auth.hook";
+import Loading from "@/src/components/UI/Loading";
 
 const LoginPage = () => {
-  const { muate: handleUserLogin, isPending } = useuserlo;
+  const { mutate: handleUserLogin, isPending } = useUserLogin();
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
   return (
     <>
-      {isp}
       <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
         <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
         <p className="mb-4">Welcome Back! Let&lsquo;s Get Started</p>
