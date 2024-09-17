46dc1785fcf75753d460f3d1ce43d756d88746ef
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-18 01:10:14 +0600

    Handling user in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,14 +16,6 @@ type Role = keyof typeof roleBasedRoutes;
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-
-  console.log(pathname);
-
-  /* const user = {
-    name: "Shariar",
-    token: "adsf asda",
-    role: "USER",
-  }; */
   const user = await getCurrentUser();
 
   if (!user) {
