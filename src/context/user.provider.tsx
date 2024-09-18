a08c3cf67702502928e423d1c57504cd10cd1187
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-18 21:52:17 +0600

    Creating a user provider layer

--- src/context/user.provider.tsx
+++ src/context/user.provider.tsx
@@ -1,7 +1,12 @@
 import { createContext, useState } from "react";
+import { IUser } from "../types";
 
 const UserContext = createContext(undefined);
 
+interface IUserProviderValue {
+    user: IUser | null
+    loading: boolean
+}
 const UserProvider = () => {
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
