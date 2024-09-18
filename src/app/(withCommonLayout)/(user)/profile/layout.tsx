af296b705b9273511d640548c3b5c83b2716ba0a
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-19 00:10:14 +0600

    user layout

--- src/app/(withCommonLayout)/(user)/profile/layout.tsx
+++ src/app/(withCommonLayout)/(user)/profile/layout.tsx
@@ -2,9 +2,13 @@ import { ReactNode } from "react";
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <h1>This is user layout</h1>
-      {children}
-    </>
+    <Container>
+      <div className="my-3 flex w-full gap-12">
+        <div className="w-2/5">
+          <Sidebar />
+        </div>
+        <div className="w-4/5">{children}</div>
+      </div>
+    </Container>
   );
 }
