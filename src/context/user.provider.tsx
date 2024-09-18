498f2ee31fe747021c3ffe867de16ae8ec2a7ba9
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-18 21:57:22 +0600

    Creating a user provider layer

--- src/context/user.provider.tsx
+++ src/context/user.provider.tsx
@@ -1,25 +1,29 @@
-import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from "react";
 import { IUser } from "../types";
 
-
 interface IUserProviderValue {
-    user: IUser | null
-    loading: boolean
-    setUser: (user: IUser | null) => void
-    setIsLoading: Dispatch<SetStateAction<boolean>>
+  user: IUser | null;
+  loading: boolean;
+  setUser: (user: IUser | null) => void;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
 }
 const UserContext = createContext<IUserProviderValue | undefined>(undefined);
 
 const UserProvider = ({ children }: { children: ReactNode }) => {
-    const [user, setUser] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-
-    return {
-        <UserContext.Provider value={{ user, setUser, isLoading, setIsLoading}} >
-        {children}
-      </UserContext.Provider>
-    }
+  return (
+    <UserContext.Provider value={{ user, setUser, isLoading, setIsLoading }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export default UserProvider;
