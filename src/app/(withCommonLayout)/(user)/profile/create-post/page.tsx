ba071e22ca04a5c5efd8c345963ad09bc0b02abd
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-19 00:44:20 +0600

    Setting up the form for the create post page

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
 export default function page() {
-  const {} = useForm();
+  const { control } = useForm();
+  const {} = useFieldArray({});
   return (
     <div>
       <h1> Create Post Page </h1>
