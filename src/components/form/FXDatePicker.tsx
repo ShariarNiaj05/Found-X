2cfa288f35dcf66d7f3b9757b76b34393d6d0a9f
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-09 22:45:37 +0600

    UI: date picker component

--- src/components/form/FXDatePicker.tsx
+++ src/components/form/FXDatePicker.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const FXDatePicker = () => {
-  return <div></div>;
+  return (
+    <Controller
+      name={name}
+      render={({ field: { value, ...fields } }) => (
+        <DatePicker
+          className="min-w-full sm:min-w-[225px]"
+          label={label}
+          variant={variant}
+          {...fields}
+        />
+      )}
+    />
+  );
 };
 
 export default FXDatePicker;
