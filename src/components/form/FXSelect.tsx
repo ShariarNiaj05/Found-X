8ca0412402bcbdbb0b8613c2bbe1d28e7fd3e570
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-09 22:48:12 +0600

    UI: select  component

---
+++ src/components/form/FXSelect.tsx
@@ -0,0 +1,17 @@
+const FXSelect = () => {
+  return (
+    <Select
+      {...register(name)}
+      className="min-w-full sm:min-w-[225px]"
+      isDisabled={disabled}
+      label={label}
+      variant={variant}
+    >
+      {options.map((option) => (
+        <SelectItem key={option.key}>{option.label}</SelectItem>
+      ))}
+    </Select>
+  );
+};
+
+export default FXSelect;
